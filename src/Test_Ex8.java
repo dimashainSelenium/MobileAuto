e84a96654c03e073d0a8f477d60436ddc2323b7b
DimaShain <dimashain@gmail.com>
2025-11-26 19:24:44 +0300

    8ое задание удалил лишнее

--- src/Test_Ex8.java
+++ src/Test_Ex8.java
@@ -6,11 +6,8 @@ import org.openqa.selenium.By;
 
 public class Test_Ex8 extends CoreTestCase {
 
-    private MainPageObject MainPageObject;
-
     protected void setUp() throws Exception{
         super.setUp();
-        MainPageObject = new MainPageObject(driver);
     }
 
     @Test
